56aee93e2c00a0221d513afb3b7189d696f16770
starraiser <490349699@qq.com>
2016-01-13 17:51:15 +0800

    fragment

---
+++ app/src/main/java/com/example/administrator/sqlite/Fragment/TimerFragment.java
@@ -0,0 +1,86 @@
+package com.example.administrator.sqlite.Fragment;
+
+import android.app.Activity;
+import android.app.ListFragment;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import com.example.administrator.sqlite.MainActivity;
+import com.example.administrator.sqlite.R;
+import com.example.administrator.sqlite.database.DBManager;
+import com.example.administrator.sqlite.entity.Item;
+import com.example.administrator.sqlite.showRecord;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class TimerFragment extends ListFragment{
+    private SimpleAdapter adapter=null;
+    private List<Item> listData;  // 数据
+    private int userId;
+    private DBManager database;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        database = new DBManager(getActivity());
+        SharedPreferences sharedPreferences = getActivity().getSharedPreferences("info", Activity.MODE_PRIVATE);  // 获取当前用户id
+        userId = sharedPreferences.getInt("userId",-1);
+
+        adapter =  new SimpleAdapter(getActivity(),getData(), R.layout.list_layout,
+                new String[]{"title","time"},new int[]{R.id.list_title,R.id.list_time});
+        setListAdapter(adapter);
+    }
+
+    private List<Map<String ,Object>> getData(){  // 获取数据库记录
+        List<Map<String,Object>> list = new ArrayList<Map<String,Object>>();
+        listData = database.getListOfUser(userId);
+        for (int i = 0; i < listData.size(); i++){
+            Map<String,Object> map = new HashMap<String,Object>();
+
+            String title = listData.get(i).getTitle();
+            String time = listData.get(i).getDate();
+            System.out.println(title);
+            map.put("title",title);
+            map.put("time",time);
+            list.add(map);
+        }
+        return list;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_record,container,false);
+        return v;
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        adapter =  new SimpleAdapter(getActivity(),getData(), R.layout.list_layout,
+                new String[]{"title","time"},new int[]{R.id.list_title,R.id.list_time});
+        setListAdapter(adapter);
+    }
+
+    @Override
+    public void onListItemClick(ListView l,View v,int position,long id){
+        int tmp = database.getListOfUser(userId).get(position).getId();  // 获取点击列表中的数据的id
+
+        Intent intent = new Intent(getActivity(), showRecord.class);
+        Bundle bundle = new Bundle();
+        bundle.putInt("id", tmp);  // 传递id到显示页面
+        intent.putExtras(bundle);
+        startActivityForResult(intent, 1);
+        //getActivity().(getActivity(),showRecord.class);
+    }
+}
--- app/src/main/java/com/example/administrator/sqlite/MainActivity.java
+++ app/src/main/java/com/example/administrator/sqlite/MainActivity.java
@@ -7,6 +7,9 @@ package com.example.administrator.sqlite;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -22,6 +25,8 @@ import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
+import com.example.administrator.sqlite.Fragment.RecordFragment;
+import com.example.administrator.sqlite.Fragment.TimerFragment;
 import com.example.administrator.sqlite.database.DBManager;
 import com.example.administrator.sqlite.entity.Item;
 import com.tandong.sa.avatars.AvatarDrawableFactory;
@@ -44,10 +49,21 @@ public class MainActivity extends Activity {
     private List<Item> listData;  // 数据
     private SimpleAdapter adapter;  // 适配器
 
+    private RecordFragment recordFragment;
+    private TimerFragment timerFragment;
+    private View recordLayout;
+    private View timerLayout;
+    private ListView recordList;
+    private ListView timerList;
+    private FragmentManager fragmentManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main);
+        init();
+        fragmentManager = getFragmentManager();
+        setTabSelection(0);
         ActivityTaskManager.getInstance().putActivity("Main", this);
         //Login.temp.finish();  // finish登录页，否则返回键会返回到登录页
 
@@ -71,7 +87,7 @@ public class MainActivity extends Activity {
 
         adapter = new SimpleAdapter(this,getData(),R.layout.list_layout,
                 new String[]{"title","time"},new int[]{R.id.list_title,R.id.list_time});
-
+/*
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {  // 列表监听器
             @Override
             public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
@@ -86,7 +102,7 @@ public class MainActivity extends Activity {
         });
 
         listView.setAdapter(adapter);  // 设置适配器
-
+*/
         newItem.setOnClickListener(new View.OnClickListener() {  // 新建按钮监听器
             @Override
             public void onClick(View v) {
@@ -192,9 +208,10 @@ public class MainActivity extends Activity {
 
     protected void onResume(){  // 重新设置适配器，刷新数据
         super.onResume();
-        SimpleAdapter newAdapter = new SimpleAdapter(this,getData(),R.layout.list_layout,
+        setTabSelection(0);
+        /*SimpleAdapter newAdapter = new SimpleAdapter(this,getData(),R.layout.list_layout,
                 new String[]{"title","time"},new int[]{R.id.list_title,R.id.list_time});
-        listView.setAdapter(newAdapter);
+        listView.setAdapter(newAdapter);*/
     }
 
     @Override
@@ -226,5 +243,78 @@ public class MainActivity extends Activity {
             return false;
         }
         return super.onKeyDown(KeyCode,event);
+    }
+
+    private void init(){
+        recordLayout = findViewById(R.id.record_layout);
+        timerLayout = findViewById(R.id.timer_layout);
+        //recordList = (ListView)findViewById(R.id.recordList);
+        //timerList = (ListView)findViewById(R.id.timerList);
+        recordLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setTabSelection(0);
+            }
+        });
+        timerLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setTabSelection(1);
+            }
+        });
+    }
+
+    private void setTabSelection(int index){
+        clearSelection();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        hideFragments(transaction);
+        switch (index){
+            case 0:
+                if(recordFragment == null){
+                    recordFragment = new RecordFragment();
+                    transaction.add(R.id.content,recordFragment);
+                } else{
+                    transaction.show(recordFragment);
+                }
+               /* recordList.setOnItemClickListener(new AdapterView.OnItemClickListener() {  // 列表监听器
+                    @Override
+                    public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+                        int id = database.getListOfUser(userId).get(arg2).getId();  // 获取点击列表中的数据的id
+
+                        Intent intent = new Intent(MainActivity.this, showRecord.class);
+                        Bundle bundle = new Bundle();
+                        bundle.putInt("id", id);  // 传递id到显示页面
+                        intent.putExtras(bundle);
+                        startActivityForResult(intent, 1);
+                    }
+                });
+
+                recordList.setAdapter(adapter);  // 设置适配器*/
+                break;
+            case 1:
+                if(timerFragment == null){
+                    timerFragment = new TimerFragment();
+                    transaction.add(R.id.content,timerFragment);
+                } else{
+                    transaction.show(timerFragment);
+                }
+                break;
+            default:
+                break;
+        }
+        transaction.commit();
+    }
+
+    private void clearSelection(){
+
+    }
+
+    private void hideFragments(FragmentTransaction transaction) {
+        if (recordFragment != null) {
+            transaction.hide(recordFragment);
+        }
+        if (timerFragment != null) {
+            transaction.hide(timerFragment);
+        }
     }
 }
